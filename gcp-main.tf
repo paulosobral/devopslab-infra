830d5b3c5bbc76e2e3a6419d07dc68a5e70bf2e8
Paulo Fernando Chaves Sobral Junior <paulo@paulosobral.com.br>
2022-06-05 18:43:09 -0300

    ajuste regiao app engine

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,6 +1,6 @@
 resource "google_app_engine_application" "app" {
   project     = "lab-devops-cloud-352100"
-  location_id = "southamerica-east1"
+  location_id = "us-central"
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
