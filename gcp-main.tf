9bce90e86cc50f5dac7005e66e1b0d1eb6069214
Paulo Fernando Chaves Sobral Junior <paulo@paulosobral.com.br>
2022-06-05 18:36:04 -0300

    Ajuste regiões

--- gcp-main.tf
+++ gcp-main.tf
@@ -1,12 +1,12 @@
 resource "google_app_engine_application" "app" {
   project     = "lab-devops-cloud-352100"
-  location_id = "us-central"
+  location_id = "southamerica-east1"
 }
 
 resource "google_artifact_registry_repository" "my-repo" {
   provider = google-beta
 
-  location = "us-central1"
+  location = "southamerica-east1"
   repository_id = "labdevops"
   description = "Imagens Docker"
   format = "DOCKER"
